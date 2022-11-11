b2c225e3f19810b68c32ad39f02e5b23d37a8c34
Luis Urrutia <luis@urrutia.me>
2022-11-11 10:06:20 -0300

    update brew installer

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ CWD="$(pwd)"
 
 which -s brew
 if [[ $? != 0 ]] ; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
   brew update
   brew upgrade
