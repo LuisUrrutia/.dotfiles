d61f644fca51e007756d1a7ee7c6b2a1861a8535
Luis Urrutia <luis@urrutia.me>
2025-08-28 12:23:33 +0200

    chore: improve install.sh

--- install.sh
+++ install.sh
@@ -6,32 +6,32 @@ if [ "$(uname)" != "Darwin" ]; then
 	exit 1
 fi
 
-STOW_FOLDERS="fish,wget,git,vim,tmux,starship,bat,btop,linearmouse,cspell"
+STOW_FOLDERS="fish,wget,git,vim,tmux,starship,bat,btop,linearmouse,cspell,atuin"
 DOTFILES="${HOME}/.dotfiles"
-
-CWD="$(pwd)"
 
 sudo -v
 while true; do
 	sudo -n true
-	sleep 30
+	sleep 20
 	kill -0 "$$" || exit
 done 2>/dev/null &
 
 # Check if brew is installed, otherwise install it
 which -s brew
 if [[ $? != 0 ]]; then
+	echo "Installing Homebrew..."
 	NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 else
+	echo "Updating Homebrew..."
 	brew update -q
 	brew upgrade -q
 fi
 
 # Load homebrew
 eval "$(/opt/homebrew/bin/brew shellenv)"
-HOMEBREW_PREFIX=$(brew --prefix)
 
-brew bundle install
+echo "Installing packages from Brewfile..."
+brew bundle install --file $DOTFILES/Brewfile
 brew cleanup
 
 uv python install
@@ -39,22 +39,21 @@ uv tool install pre-commit --with pre-commit-uv
 
 [ ! -L "${HOMEBREW_PREFIX}/bin/sha256sum" ] && ln -s "${HOMEBREW_PREFIX}/bin/gsha256sum" "${HOMEBREW_PREFIX}/bin/sha256sum"
 
+echo "Installing fzf..."
 $HOMEBREW_PREFIX/opt/fzf/install --all --no-bash --no-zsh --no-fish --no-update-rc --key-bindings --completion
-
-# Configure ZSH and config files
-# rm $HOME/.zshrc $HOME/.zshenv $HOME/.zlogin
-
-# Remove fish config if it exists because we will use stow to add stuff
-rm -rf $HOME/.config/fish/
 
 for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g"); do
 	echo "stow $folder"
 	stow -D $folder
-	stow $folder -t $HOME
+	stow --adopt $folder -t $HOME
 done
+
+# Stow adopt could override some files, so we need to restore them
+git checkout .
 
 bat cache --build
 
+echo "Configuring iTerm2..."
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES/iterm"
 defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 
@@ -73,8 +72,12 @@ sh cursor.sh
 # fi
 
 # Add fish to shells and set it as default
+echo "Setting fish as default shell..."
 sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 chsh -s /opt/homebrew/bin/fish
+
+# Install fish plugins
+/opt/homebrew/bin/fish -C "fisher update"
 
 echo "Installation complete!"
 echo "Please restart your terminal to apply changes."
