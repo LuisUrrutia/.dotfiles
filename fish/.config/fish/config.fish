e2be7c40195d17d2ff71ef1cfeb0b4da0e1ee826
Luis Urrutia <luis@urrutia.me>
2025-08-28 12:22:10 +0200

    chore: configs moved to conf.d

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -g fish_greeting custom_greeting
-
-set -Ux HOMEBREW_NO_ANALYTICS 1
 
 set -gx GPG_TTY $(tty)
 set -gx LANG en_US.UTF-8
-set -gx EDITOR nvim
-set -gx VISUAL nvim
 set -gx PAGER less
 set -gx PNPM_HOME "$HOME/Library/pnpm"
-
-set -g fish_key_bindings fish_vi_key_bindings
 
 # fish_config theme save "Catppuccin Mocha"
 set -Ux FZF_DEFAULT_OPTS "\
@@ -27,4 +20,3 @@ set fzf_history_time_format %d-%m-%y
 
 starship init fish | source
 zoxide init fish | source
-fzf --fish | source
