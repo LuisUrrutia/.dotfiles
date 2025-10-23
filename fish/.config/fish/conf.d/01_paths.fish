00e192a105dd9e74c1c010db36a636670fb45782
Luis Urrutia <luis@urrutia.me>
2025-10-23 14:58:11 +0200

    feat(fish): add gnu-tar binaries to PATH

--- fish/.config/fish/conf.d/01_paths.fish
+++ fish/.config/fish/conf.d/01_paths.fish
@@ -1,3 +1,3 @@
 # Add local bins and gnu-sed/grep to path
-fish_add_path --prepend --path --move "$HOME/.local/bin" "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin" "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin" "$HOMEBREW_PREFIX/opt/rustup/bin"
+fish_add_path --prepend --path --move "$HOME/.local/bin" "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" "$HOMEBREW_PREFIX/opt/gnu-sed/libexec/gnubin" "$HOMEBREW_PREFIX/opt/gnu-tar/libexec/gnubin" "$HOMEBREW_PREFIX/opt/grep/libexec/gnubin" "$HOMEBREW_PREFIX/opt/rustup/bin"
 fish_add_path --append --path --move $PNPM_HOME "$HOMEBREW_PREFIX/opt/openjdk/bin" "$HOME/.rvm/bin:$BUN_INSTALL/bin"
