2ccf55fe467dfda7c04d3ae9fb6b0843be646fb1
Luis Urrutia <luis@urrutia.me>
2025-12-20 23:10:50 +0100

    feat(fish): add neovim lazy plugin sync to upup

--- fish/.config/fish/functions/upup.fish
+++ fish/.config/fish/functions/upup.fish
@@ -2,4 +2,5 @@ function upup -d "updates different tools"
     brew update && brew upgrade && brew autoremove && brew cleanup --prune=all && brew doctor
     pnpm self-update
     /opt/homebrew/bin/fish -C "fisher update"
+    nvim --headless "+Lazy! sync" +qa
 end
