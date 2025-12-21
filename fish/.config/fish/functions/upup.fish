4805ef1192a8b260f45642f5cdfd88c4dd68794d
Luis Urrutia <luis@urrutia.me>
2025-12-21 22:58:06 +0100

    feat(fish): move fisher update to end of upup function

--- fish/.config/fish/functions/upup.fish
+++ fish/.config/fish/functions/upup.fish
@@ -1,6 +1,8 @@
 function upup -d "updates different tools"
     brew update && brew upgrade && brew autoremove && brew cleanup --prune=all && brew doctor
     pnpm self-update
-    /opt/homebrew/bin/fish -C "fisher update"
     nvim --headless "+Lazy! sync" +qa
+
+    # Fish update should be at the end of the function
+    /opt/homebrew/bin/fish -C "fisher update"
 end
