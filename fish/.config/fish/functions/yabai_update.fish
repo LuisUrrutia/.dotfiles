4fa1de99c094b5cbfaa12bdd65adef02d2e4b663
Luis Urrutia <luis@urrutia.me>
2025-12-21 22:19:47 +0100

    feat(fish): add yabai_update function for sudoers permissions

---
+++ fish/.config/fish/functions/yabai_update.fish
@@ -0,0 +1,4 @@
+function yabai_update
+    echo "Updating yabai permissions..."
+    echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
+end
