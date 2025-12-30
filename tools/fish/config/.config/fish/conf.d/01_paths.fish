b01e0f2f1b8c24c53669b6b0703bc78231fb3ec7
Luis Urrutia <luis@urrutia.me>
2025-12-30 20:39:28 +0100

    chore(fish): migrate to fish 4.3 and add opencode path

--- tools/fish/config/.config/fish/conf.d/01_paths.fish
+++ tools/fish/config/.config/fish/conf.d/01_paths.fish
@@ -1,3 +1,3 @@
 # Add local bins and gnu-sed/grep to path
 fish_add_path --prepend --path --move "$HOME/.local/bin" "$HOMEBREW_PREFIX/bin" "$HOMEBREW_PREFIX/sbin" "$HOMEBREW_PREFIX/opt/rustup/bin"
-fish_add_path --append --path --move "$HOME/Library/pnpm"  "$HOME/.rvm/bin" "$HOME/.foundry/bin" "$HOMEBREW_PREFIX/opt/openjdk/bin"
+fish_add_path --append --path --move "$HOME/Library/pnpm"  "$HOME/.rvm/bin" "$HOME/.foundry/bin" "$HOMEBREW_PREFIX/opt/openjdk/bin" "$HOME/.opencode/bin"
