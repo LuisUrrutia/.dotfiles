4a0e28bc688092ca59fa89a24c46a9120f7113c3
Luis Urrutia <luis@urrutia.me>
2026-04-29 14:38:30 +0200

    chore(git): copy git wrapper during install

--- tools/git/install.sh
+++ tools/git/install.sh
@@ -5,3 +5,11 @@ source "${DOTFILES:-$HOME/.dotfiles}/tools/lib.sh"
 require_brew_bin git
 
 stow_config git
+
+if [[ -L "$HOME/.local/bin" ]]; then
+  rm "$HOME/.local/bin"
+fi
+mkdir -p "$HOME/.local/bin"
+rm -f "$HOME/.local/bin/git"
+install -m 0755 "$DOTFILES/tools/git/bin/git" "$HOME/.local/bin/git"
+echo "Installed git wrapper"
