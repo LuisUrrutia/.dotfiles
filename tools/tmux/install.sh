856b78a26e51c983b03efce1a2b617ef96fc9020
Luis Urrutia <luis@urrutia.me>
2026-04-29 14:41:03 +0200

    chore(tmux): modernize tmux setup

--- tools/tmux/install.sh
+++ tools/tmux/install.sh
@@ -3,5 +3,17 @@
 source "${DOTFILES:-$HOME/.dotfiles}/tools/lib.sh"
 
 require_brew_bin tmux
+tmux_bin_path="$bin_path"
+require_brew_bin git
+git_bin_path="$bin_path"
+
+tpm_dir="$HOME/.tmux/plugins/tpm"
+if [[ ! -d "$tpm_dir" ]]; then
+  mkdir -p "$(dirname "$tpm_dir")"
+  "$git_bin_path" clone https://github.com/tmux-plugins/tpm "$tpm_dir"
+fi
 
 stow_config tmux
+
+"$tmux_bin_path" source-file "$HOME/.tmux.conf" 2>/dev/null || true
+"$tpm_dir/bin/install_plugins"
