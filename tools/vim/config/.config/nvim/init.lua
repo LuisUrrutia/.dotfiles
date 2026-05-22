187ccba663ef4d6fa580a31482064cb5b9278273
Luis Urrutia <luis@urrutia.me>
2026-05-22 10:57:56 +0200

    chore(nvim): update hover keymap and plugins

--- tools/vim/config/.config/nvim/init.lua
+++ tools/vim/config/.config/nvim/init.lua
@@ -4,3 +4,8 @@ require("config.treesitter")
 
 vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gh", "<cmd>diffget //3<CR>")
+vim.keymap.set("n", "K", function()
+    if next(vim.lsp.get_clients({ bufnr = 0 })) ~= nil then
+        vim.lsp.buf.hover()
+    end
+end, { desc = "Show LSP hover" })
