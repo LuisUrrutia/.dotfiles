ec5baf23438fb4db6d8ec6bfe81d873821d04b7d
Luis Urrutia <luis@urrutia.me>
2026-05-31 17:49:19 +0200

    style(fish): format clipboard helper

--- tools/fish/config/.config/fish/functions/cb.fish
+++ tools/fish/config/.config/fish/functions/cb.fish
@@ -2,7 +2,7 @@ function cb -d 'Gets or sets the system clipboard content'
     # If we are likely not the start of a pipeline then read stdin into the
     # clipboard.
     if not isatty stdin
-        fish_clipboard_copy 2> /dev/null
+        fish_clipboard_copy 2>/dev/null
     end
 
     # Always echo the current value of the clipboard.
